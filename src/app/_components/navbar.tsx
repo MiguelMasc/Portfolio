079474066f86d967924082ae84d82bf51878d571
Miguel <memiguel1314@gmail.com>
2025-06-18 14:40:20 -0700

    added hidden navbar with home and contact links

---
+++ src/app/_components/navbar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const NavBar  = () => (
+  <nav className="group fixed top-0 left-0 h-screen w-8 hover:w-auto px-6 bg-transparent flex flex-col justify-center">
+    <div className="flex-col items-center group-hover:items-start hidden group-hover:flex"><Link href="/" className="text-white py-4">Home</Link>
+      {/* <Link href="/projects" className="text-white py-4">Projects</Link> */}
+      <Link href="/contact" className="text-white py-4">Contact</Link>
+    </div>
+  </nav>
+);
+
+export default NavBar;
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <h1 className="text-4xl 
+                   font-bold
+                   flex
+                   justify-center
+                   items-center
+                   h-screen">
+      Contact Page!
+    </h1>
+  );
+}
