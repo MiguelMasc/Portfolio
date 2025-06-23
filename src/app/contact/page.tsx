a6f7473ed4fda712a4ad30b7f014f3cb3d95a013
Miguel61823 <mmascare@ucsc.edu>
2025-06-23 14:36:30 -0700

    contact page reacts to window size

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,26 +1,29 @@
+"use client";
 
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
 
 export default function Page() {
   return (
-    <div className="flex flex-col justify-center min-h-screen w-3/5 mx-auto">
+    <div className="flex flex-col justify-center min-h-screen sm:w-3/5 w-screen p-8 mx-auto">
       <h1 className="text-4xl 
                     font-bold
                     flex">
         Contact Me!
       </h1>
-      <div className=" mt-6">
-        I&apos;m always open to discussing new projects, creative ideas, or opportunities to be part of your vision.
-        <div className="flex flex-col gap-2 mt-6">
+      <div className="mt-6 ">
+        I&apos;m always open to discussing new projects, creative ideas, or opportunities to be part of your vision. 
+        <div className="flex w-full flex-col gap-2 mt-6 justify-between">
           <Button variant="outline"
-                  className="flex items-center justify-center">
-            <FaGithub className="mr-2" />
+                  className="flex justify-start sm:w-auto"
+                  onClick={() => window.open("https://github.com/MiguelMasc", "_blank")}>
+            <FaGithub className="m-2" />
             MiguelMasc 
           </Button>
           <Button variant="outline"
-                  className="flex items-center justify-center">
-            <FaLinkedin className="mr-2" size="xlarge"/>
+                  className="flex justify-start sm:w-auto"
+                  onClick={() => window.open("https://www.linkedin.com/in/miguel-mascareno/", "_blank")}>
+            <FaLinkedin className="m-2"/>
             Miguel Mascareno
           </Button>
         </div>
